63ca62518641853249bfc43e7a809e34e9bc30d9
harsh231671 <115443950+harsh231671@users.noreply.github.com>
2026-03-12 21:45:37 +0530

    context added todo context local project

--- 09_todo_context_local_project/src/App.jsx
+++ 09_todo_context_local_project/src/App.jsx
@@ -9,6 +9,26 @@ function App() {
         setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
     };
 
+    const updateTodo = (id, todo) => {
+        setTodos((prev) =>
+            prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)),
+        );
+    };
+
+    const deleteTodo = (id) => {
+        setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    };
+
+    const toggleComplete = (id) => {
+        setTodos((prev) =>
+            prev.map((prevTodo) =>
+                prevTodo.id === id
+                    ? { ...prevTodo, completed: !prevTodo.completed }
+                    : prevTodo,
+            ),
+        );
+    };
+
     return (
         <TodoProvider
             value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
